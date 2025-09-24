4295f6243bbc552e261b195c73e5050588599eda
João <joaovictorro102@gmail.com>
2025-09-24 15:25:16 -0400

    Feat: adição do README e da rota do get-rooms no projeto

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,13 +1,15 @@
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../DB/connection.ts'
 import { schema } from '../../DB/schema/index.ts'
 
-export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
+export const getRoomsRoute: FastifyPluginCallbackZod =  (app) => {
     app.get('/rooms', async () => {
       const results = await   db.select({
         id: schema.rooms.id,
         name: schema.rooms.name,
         
       }).from(schema.rooms).orderBy(schema.rooms.createdAt)
+
+      return results
     })
 }
